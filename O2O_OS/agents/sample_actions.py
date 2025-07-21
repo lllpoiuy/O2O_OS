1f3a67cc29390de862f458760c8bca5a1b6b0354
Lin Shenghan <86179118@qq.com>
2025-07-21 23:14:23 +0800

    feat: IBRL

--- O2O_OS/agents/sample_actions.py
+++ O2O_OS/agents/sample_actions.py
@@ -105,12 +105,17 @@ def sample_distill_ddpg(
     Example config:
     "sample_actions":{
         "type": "distill_ddpg",
-        "actor_num_samples": 10
+        "actor_num_samples": 32,
+        "soft_max": true,
+        "imitation_boostrapped": true
     }
     """
     assert agent.network.select('actor_onestep_flow') is not None
     assert agent.network.select('critic') is not None
+    assert agent.config['sample_actions'].get('type') is not None
     assert agent.config['sample_actions'].get('actor_num_samples') is not None
+    assert agent.config['sample_actions'].get('soft_max') is not None
+    assert agent.config['sample_actions'].get('imitation_boostrapped') is not None
     
     action_dim = agent.config['action_dim'] * agent.config['horizon_length']
     num_samples = agent.config['sample_actions']['actor_num_samples']
@@ -143,5 +148,37 @@ def sample_distill_ddpg(
     indices = indices.reshape(-1)
     bsize = len(indices)
     actions = jnp.reshape(actions, (-1, num_samples, action_dim))[jnp.arange(bsize), indices, :].reshape(bshape + (action_dim,))
+    q_actions = jnp.reshape(q, (-1, num_samples))[jnp.arange(bsize), indices].reshape(bshape)
+
+    if agent.config['sample_actions']['imitation_boostrapped']:
+        imitation_noise = jax.random.normal(
+            rng,
+            (
+                *observations.shape[: -len(agent.config['ob_dims'])],
+                1, action_dim
+            ),
+        )
+        imitation_observations = jnp.repeat(observations[..., None, :], 1, axis=-2)
+        imitation_actions = compute_flow_actions(agent, imitation_observations, imitation_noise)
+        imitation_actions = jnp.clip(imitation_actions, -1, 1)
+
+        q_imitation = agent.network.select("critic")(imitation_observations, imitation_actions)
+        if agent.config["critic_loss"]["q_agg"] == "mean":
+            q_imitation = q_imitation.mean(axis=0)
+        else:
+            q_imitation = q_imitation.min(axis=0)
+
+        q_imitation = jnp.squeeze(q_imitation, axis=1)
+        imitation_actions = jnp.squeeze(imitation_actions, axis=1)
+
+        # print("q_imitation.shape:", q_imitation.shape)
+        # print("q_actions.shape:", q_actions.shape)
+        # print("imitation_actions.shape:", imitation_actions.shape)
+        # print("actions.shape:", actions.shape)
+
+        q_actions = jnp.where(
+            jnp.expand_dims(q_imitation, axis=-1) > jnp.expand_dims(q_actions, axis=-1),
+            imitation_actions, actions
+        )
     
     return actions
