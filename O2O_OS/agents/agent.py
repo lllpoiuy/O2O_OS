820c25d7af76db688b6a35309a190de603fa0bd7
Lin Shenghan <86179118@qq.com>
2025-07-30 20:19:40 +0800

    remove asset & fix agent

--- O2O_OS/agents/agent.py
+++ O2O_OS/agents/agent.py
@@ -101,15 +101,15 @@ class O2O_OS_Agent(flax.struct.PyTreeNode):
     @staticmethod
     def _update(self, batch):
         """Update the agent and return a new agent with information dictionary."""
-        new_rng, rng = jax.random.split(agent.rng)
+        new_rng, rng = jax.random.split(self.rng)
 
         def loss_fn(grad_params):
             return self.total_loss(batch, grad_params, rng=rng)
 
-        new_network, info = agent.network.apply_loss_fn(loss_fn=loss_fn)
-        agent.target_update(new_network, 'critic')
+        new_network, info = self.network.apply_loss_fn(loss_fn=loss_fn)
+        self.target_update(new_network, 'critic')
 
-        return agent.replace(network=new_network, rng=new_rng), info
+        return self.replace(network=new_network, rng=new_rng), info
     
     @jax.jit
     def update(self, batch):
